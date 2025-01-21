eccc124dd297e447d5d37529b1955fd38a69e61c
llach <lachluca@gmail.com>
2025-01-21 11:11:18 +0100

    add normalization

--- unstack/test_pusht.py
+++ unstack/test_pusht.py
@@ -32,75 +32,72 @@ dataset = PushTImageDataset(
     zarr_path=dataset_path,
     horizon=pred_horizon,
 )
+normalizer = dataset.get_normalizer()
 
-print("eval")
-exit(0)
+for i in tqdm(range(max_steps), desc="Eval PushTImageEnv"):
+    B = 1
 
+    sample = dataset[i]
 
-with tqdm(total=max_steps, desc="Eval PushTImageEnv") as pbar:
-    while not False:
-        B = 1
-        # stack the last obs_horizon number of observations
-        images = np.stack([x['image'] for x in obs_deque])
-        agent_poses = np.stack([x['agent_pos'] for x in obs_deque])
+    # normalized inputs and outputs
+    nimages = sample['obs']['image'] # images are normalized in sampling fn
+    nagent_poses = normalizer.normalize({"agent_pos": sample['obs']['agent_pos']})
 
-        # normalize observation
-        nagent_poses = normalize_data(agent_poses, stats=stats['agent_pos'])
-        # images are already normalized to [0,1]
-        nimages = images
+    # device transfer
+    nimages = torch.from_numpy(nimages).to(device, dtype=torch.float32)
+    # (2,3,96,96)
+    nagent_poses = torch.from_numpy(nagent_poses).to(device, dtype=torch.float32)
+    # (2,2)
 
-        # device transfer
-        nimages = torch.from_numpy(nimages).to(device, dtype=torch.float32)
-        # (2,3,96,96)
-        nagent_poses = torch.from_numpy(nagent_poses).to(device, dtype=torch.float32)
-        # (2,2)
+    print("eval")
+    exit(0)
 
-        # infer action
-        with torch.no_grad():
-            # get image features
-            image_features = ema_nets['vision_encoder'](nimages)
-            # (2,512)
+    # infer action
+    with torch.no_grad():
+        # get image features
+        image_features = ema_nets['vision_encoder'](nimages)
+        # (2,512)
 
-            # concat with low-dim observations
-            obs_features = torch.cat([image_features, nagent_poses], dim=-1)
+        # concat with low-dim observations
+        obs_features = torch.cat([image_features, nagent_poses], dim=-1)
 
-            # reshape observation to (B,obs_horizon*obs_dim)
-            obs_cond = obs_features.unsqueeze(0).flatten(start_dim=1)
+        # reshape observation to (B,obs_horizon*obs_dim)
+        obs_cond = obs_features.unsqueeze(0).flatten(start_dim=1)
 
-            # initialize action from Guassian noise
-            noisy_action = torch.randn(
-                (B, pred_horizon, action_dim), device=device)
-            naction = noisy_action
+        # initialize action from Guassian noise
+        noisy_action = torch.randn(
+            (B, pred_horizon, action_dim), device=device)
+        naction = noisy_action
 
-            # init scheduler
-            noise_scheduler.set_timesteps(num_diffusion_iters)
+        # init scheduler
+        noise_scheduler.set_timesteps(num_diffusion_iters)
 
-            for k in noise_scheduler.timesteps:
-                # predict noise
-                noise_pred = ema_nets['noise_pred_net'](
-                    sample=naction,
-                    timestep=k,
-                    global_cond=obs_cond
-                )
+        for k in noise_scheduler.timesteps:
+            # predict noise
+            noise_pred = ema_nets['noise_pred_net'](
+                sample=naction,
+                timestep=k,
+                global_cond=obs_cond
+            )
 
-                # inverse diffusion step (remove noise)
-                naction = noise_scheduler.step(
-                    model_output=noise_pred,
-                    timestep=k,
-                    sample=naction
-                ).prev_sample
+            # inverse diffusion step (remove noise)
+            naction = noise_scheduler.step(
+                model_output=noise_pred,
+                timestep=k,
+                sample=naction
+            ).prev_sample
 
-        # unnormalize action
-        naction = naction.detach().to('cpu').numpy()
-        # (B, pred_horizon, action_dim)
-        naction = naction[0]
-        action_pred = unnormalize_data(naction, stats=stats['action'])
+    # unnormalize action
+    naction = naction.detach().to('cpu').numpy()
+    # (B, pred_horizon, action_dim)
+    naction = naction[0]
+    action_pred = unnormalize_data(naction, stats=stats['action'])
 
-        # only take action_horizon number of actions
-        start = obs_horizon - 1
-        end = start + action_horizon
-        action = action_pred[start:end,:]
-        # (action_horizon, action_dim)
+    # only take action_horizon number of actions
+    start = obs_horizon - 1
+    end = start + action_horizon
+    action = action_pred[start:end,:]
+    # (action_horizon, action_dim)
 
 
 # print out the maximum target coverage
