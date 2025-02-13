08d7f059286b7284cba9bfb163b17bf5f4c8307f
Luca Michael Lach <llach@iri.upc.edu>
2025-02-13 11:11:11 +0100

    add pose collection, wait for data

--- robot/robot_node.py
+++ robot/robot_node.py
@@ -27,11 +27,13 @@ class DiffusionPolicyNode(Node):
         super().__init__('diffusion_policy_node')
         
         self.declare_parameter('rate', 15)
+        self.declare_parameter('obs_hist', 2)
         self.declare_parameter('device', 'gpu')
         self.declare_parameter('img_shape', [96,96])
         
         self.rate = self.get_parameter('rate').get_parameter_value().integer_value
         self.device = self.get_parameter('device').get_parameter_value().string_value
+        self.obs_hist = self.get_parameter('obs_hist').get_parameter_value().integer_value
         self.img_shape = self.get_parameter('img_shape').get_parameter_value().integer_array_value
         
         self.cbg = ReentrantCallbackGroup()
@@ -42,47 +44,49 @@ class DiffusionPolicyNode(Node):
         
         self.bridge = CvBridge()
 
-        # Initialize buffers for observations with history of 2
+        # Initialize buffers for observations 
         self.rgb_lock = Lock()
         self.pose_lock = Lock()
-        self.camera_buffer = deque(maxlen=2)
-        self.position_buffer = deque(maxlen=2)
+        self.camera_buffer = deque(maxlen=self.obs_hist)
+        self.position_buffer = deque(maxlen=self.obs_hist)
         
         while not self.tf_buffer.can_transform("map", "wrist_3_link", rclpy.time.Time()):
             print("waiting for tf ...")
             time.sleep(0.05)
             rclpy.spin_once(self)
+            
         self.Tstart = get_trafo("map", "wrist_3_link", self.tf_buffer)
         
         self.img_sub = self.create_subscription(
             CompressedImage, "/camera/color/image_raw/compressed", self.rgb_cb, 0, callback_group=self.cbg
         )
         
+        # periodically update robot pose (here: relative to starting pose)
         self.create_timer(1/(self.rate*2), self.update_pose, self.cbg)
-        
 
         # self.load_policy()
         
-        # TODO get tf
-        
     def rgb_cb(self, msg):
-        with self.rgb_lock:
-            try:
-                # convert image to cv2, resize and normalize TODO do we have to rotate here?
-                frame = self.bridge.compressed_imgmsg_to_cv2(msg, desired_encoding='rgb8')
-                resized_frame = cv2.resize(frame, self.img_shape, interpolation=cv2.INTER_AREA)
-                resized_frame = resized_frame.astype(np.float32) / 255.0
+        try:
+            # convert image to cv2, resize and normalize 
+            frame = self.bridge.compressed_imgmsg_to_cv2(msg, desired_encoding='rgb8')
+            resized_frame = cv2.resize(frame, self.img_shape, interpolation=cv2.INTER_AREA)
+            resized_frame = resized_frame.astype(np.float32) / 255.0
+    
+            frame_tensor = torch.from_numpy(np.transpose(resized_frame, (2, 0, 1)))  # channels first
+        except CvBridgeError as e:
+            self.get_logger().error(f"CvBridge Error: {e}")
+            return
         
-                frame_tensor = torch.from_numpy(np.transpose(resized_frame, (2, 0, 1)))  # channels first
-                self.camera_buffer.append(frame_tensor)
-            except CvBridgeError as e:
-                self.get_logger().error(f"CvBridge Error: {e}")
-                return
+        with self.rgb_lock:
+            self.camera_buffer.append(frame_tensor)
             
     def update_pose(self):
+        Tnow = get_trafo("map", "wrist_3_link", self.tf_buffer)
+        pos_rel = Tnow[:3,3] + inv(self.Tstart)[:3,3]
+        
         with self.pose_lock:
-            Tnow = get_trafo("map", "wrist_3_link", self.tf_buffer)
-            print(Tnow@inv(self.Tstart))
+            self.position_buffer.appendleft(pos_rel)
         
     def load_policy(self):
         print("loading policy ....")
@@ -91,7 +95,7 @@ class DiffusionPolicyNode(Node):
         cfg = payload['cfg']
         cls = hydra.utils.get_class(cfg._target_)
 
-        workspace : TrainDiffusionUnetHybridWorkspace = cls(cfg, output_dir="/tmp")
+        workspace: TrainDiffusionUnetHybridWorkspace = cls(cfg, output_dir="/tmp")
         workspace.load_payload(payload, exclude_keys=None, include_keys=None)
 
         # get policy from workspace
@@ -106,8 +110,8 @@ class DiffusionPolicyNode(Node):
         
     def execute(self):
         r = self.create_rate(self.rate)
-        while True:
-            print("still waiting ...")
+        while len(self.position_buffer) < self.obs_hist and len(self.camera_buffer) < self.obs_hist:
+            print('waiting for obs ...')
             r.sleep()
 
 def main(args=None):
